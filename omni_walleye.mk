5a7d647761e074a8f02a37cc163ec56c6ed435e1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-27 18:08:09 +0200

    walleye: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: google Model: MBOX Script version: 3.0.0

---
+++ omni_walleye.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from walleye device
+$(call inherit-product, device/google/walleye/device.mk)
+
+PRODUCT_DEVICE := walleye
+PRODUCT_NAME := omni_walleye
+PRODUCT_BRAND := google
+PRODUCT_MODEL := MBOX
+PRODUCT_MANUFACTURER := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="eros_p1-userdebug 10 QP1A.191105.004 eng.cmj.20240417.085715 test-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:10/QP1A.191105.004/5908170:user/release-keys
