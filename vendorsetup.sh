90da6bd3288043226fbf58bb7969a51c091c8879
XvSvM <vadiagadia@gmail.com>
2024-06-04 03:07:24 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_walleye-user
-add_lunch_combo omni_walleye-userdebug
-add_lunch_combo omni_walleye-eng
+add_lunch_combo twrp_walleye-user
+add_lunch_combo twrp_walleye-userdebug
+add_lunch_combo twrp_walleye-eng
