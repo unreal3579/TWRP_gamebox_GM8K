9f33df5a3e6ed280484c04d2f1e4645940f70949
XvSvM <vadiagadia@gmail.com>
2024-06-06 18:37:22 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,7 @@ BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := firmware_class.path=/vendor/etc/firmware firmware_class.path=/vendor/etc/firmware selinux=1 androidboot.selinux=permissive androidboot.dtbo_idx=0,1,2 buildvariant=userdebug
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02f88000
+BOARD_KERNEL_TAGS_OFFSET := 0xfff88100
 BOARD_DTB_OFFSET := 0xfff88100
 TARGET_KERNEL_ARCH := arm
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
